5b11317a5a93f459a9ffcf988e52dc6bdf68d79f
kampolaczek <kampolaczek@gmail.com>
2019-09-21 18:25:21 +0200

    ćwiczenie Dependency Injection

---
+++ NetFramework/DependencyInjection/ConsoleUI/ConteinerConfig.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public static class ConteinerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<BetterBusinessLogic>().As<IBusinessLogic>();
+
+            builder.RegisterType<Application>().As<IApplication>();
+
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(t => t.Namespace.Contains("Utilities"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name =="I"+ t.Name));
+
+            return builder.Build();
+        }
+    }
+}
---
+++ NetFramework/DependencyInjection/ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conteiner = ConteinerConfig.Configure();
+
+            using (var scope = conteiner.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
