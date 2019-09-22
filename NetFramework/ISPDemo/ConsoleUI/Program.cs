b58cf483c3c0f6217b03dfc2363564da6b487b59
kampolaczek <kampolaczek@gmail.com>
2019-09-22 23:18:12 +0200

    ćw - Interface Segregation- I

---
+++ NetFramework/ISPDemo/ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBorrowableDVD dvd = new DVD
+            {
+                Borrower = "asd"
+            };
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NetFramework/ISPDemo/DemoLibrary/AudioBook/IAudioBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    interface IAudioBook :ILibraryItem
+    {
+        int RuntimeInMinutes { get; set; }
+
+    }
+}
